f64048d5201e1db2137f4dd3b824e7f3bea21854
Ming_Yu <108409934+MingYu5@users.noreply.github.com>
2025-04-12 15:08:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -196,8 +196,7 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/driver"
 	"github.com/wdvxdr1123/ZeroBot/message"
 
-	// webctrl 
-	"github.com/FloatTech/zbputils/control/web"
+	// webctrl "github.com/FloatTech/zbputils/control/web"
 
 	"github.com/FloatTech/ZeroBot-Plugin/kanban/banner"
 	// -----------------------以上为内置依赖，勿动------------------------ //
@@ -260,7 +259,7 @@ func init() {
 	// sus = append(sus, 87654321)
 
 	// 启用 webui
-	go webctrl.RunGui(*g)
+	// go webctrl.RunGui(*g)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
